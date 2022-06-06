d80848380fd0657096a6f9876743de0239573662
Pablo Cordero <pablocorderoromero@gmail.com>
2022-06-07 00:48:40 +0200

    asdfasd

--- granada.py
+++ granada.py
@@ -14,46 +14,52 @@ def get_regex():
     
     di = r"\d{2}"
     sec = r"\d{3}"
+    su = r"\d{2}|"
     m = r"[ABCU]"
     let = r"[A-Z]\-[A-Z]"
     code = r"\d{9}"
     num = r"\d{2}"
-    local_name = r"(?:\w| )+?"
-    street = r"(?:\w| |\d)+?"
+    local_name = r"(?:\S| )+?"
+    street = r"(?:\S| |\d)+?"
+    optional_place = r"(?:\S| )+?"
     postal_code = r"\d{5}"
-    town = r"(?:\w| )+?"
+    town = r"(?:\S| )+?"
     province = r"(?:\w| )+?"
     
-    # (\d{2})\s+(\d{3})\s+([ABCU])\s+([A-Z]\-[A-Z])\s+(\d{9})\s+(\d{2})\s+((?:\w| )+?)\s{2,}((?:\w| |\d)+?)\s{2,}(\d{5})\s((?:\w| )+?)\s{2,}((?:\w| )+?)\s{2,}
+    # (\d{2})\s+(\d{3})\s+(\d{2}|)\s+([ABCU])\s+([A-Z]\-[A-Z])\s+(\d{9})\s+(\d{2})\s+((?:\S| )+?)\s{2,}((?:\S| |\d)+?)\s{2,}((?:(?:\S| )+?|)?)\s{2,}(\d{5})\s+((?:\S| )+?)\s{2,}((?:\w| )+?)\s{2,}
     regex = (
-        rf"({di})",
-        AT_LEAST_ONE_SPACE,
-        rf"({sec})",
-        AT_LEAST_ONE_SPACE,
-        rf"({m})",
-        AT_LEAST_ONE_SPACE,
-        rf"({let})",
-        AT_LEAST_ONE_SPACE,
-        rf"({code})",
-        AT_LEAST_ONE_SPACE,
-        rf"({num})",
-        AT_LEAST_ONE_SPACE,
-        rf"({local_name})",
-        AT_LEAST_TWO_SPACES,
-        rf"({street})",
-        AT_LEAST_TWO_SPACES,
-        rf"({postal_code})",
-        AT_LEAST_ONE_SPACE,
-        rf"({town})",
-        AT_LEAST_TWO_SPACES,
-        rf"({province})",
-        AT_LEAST_TWO_SPACES,
+        rf"({di})", AT_LEAST_ONE_SPACE,
+        rf"({sec})", AT_LEAST_ONE_SPACE,
+        rf"({su})", AT_LEAST_ONE_SPACE,
+        rf"({m})", AT_LEAST_ONE_SPACE,
+        rf"({let})", AT_LEAST_ONE_SPACE,
+        rf"({code})", AT_LEAST_ONE_SPACE,
+        rf"({num})", AT_LEAST_ONE_SPACE,
+        rf"({local_name})", AT_LEAST_TWO_SPACES,
+        rf"({street})", AT_LEAST_TWO_SPACES,
+        rf"({optional_place})", AT_LEAST_TWO_SPACES,
+        rf"({postal_code})", AT_LEAST_ONE_SPACE,
+        rf"({town})", AT_LEAST_TWO_SPACES,
+        rf"({province})", AT_LEAST_TWO_SPACES,
     )
     return "".join(regex)
     
-
+def create_csv_from_list(data: list):
+    output = "\n".join([",".join(row) for row in data])
+    with open("data.csv", "w") as f:
+        f.write(output)
 
 if __name__ == "__main__":
+    print("Getting text...")
     text = get_text_from_pdf("boletin.pdf")
+
+    print("Executing regex...")
     regex = get_regex()
-    print(re.findall(regex, text)[:3])
+    data = re.findall(regex, text)
+
+    print(f"Extracted {len(data)} rows")
+    print(f"Creating CSV...")
+    create_csv_from_list(data)
+    
+    
+    
