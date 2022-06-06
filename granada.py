e5836775294c41703ee1d9309cb141cbe96424c6
Pablo Cordero <pablocorderoromero@gmail.com>
2022-06-06 23:36:02 +0200

    Create first version of extracting script

--- granada.py
+++ granada.py
@@ -1,12 +1,59 @@
 import slate3k as slate
+import re
 
-regex = r"(\d{2})\s+(\d{3})\s+([ABCU])\s+([A-Z]\-[A-Z])\s+(\d{9})\s+(\d{2})\s+((?:[A-Z]| )+?)\s{2,}"
+def get_text_from_pdf(filename: str) -> str:
+    with open(filename, "rb") as pdf_file:
+        pages = slate.PDF(pdf_file)
 
-with open("boletin.pdf", "rb") as pdf_file:
-    pages = slate.PDF(pdf_file)
-    with open("data.txt", "a") as write_file:
-        for page in pages:
-            string = page
-            string += "\n"
-            write_file.write(string)
+    return "\n".join(pages)    
+
+
+def get_regex():
+    AT_LEAST_ONE_SPACE = r"\s+"
+    AT_LEAST_TWO_SPACES = r"\s{2,}"
     
+    di = r"\d{2}"
+    sec = r"\d{3}"
+    m = r"[ABCU]"
+    let = r"[A-Z]\-[A-Z]"
+    code = r"\d{9}"
+    num = r"\d{2}"
+    local_name = r"(?:\w| )+?"
+    street = r"(?:\w| |\d)+?"
+    postal_code = r"\d{5}"
+    town = r"(?:\w| )+?"
+    province = r"(?:\w| )+?"
+    
+    # (\d{2})\s+(\d{3})\s+([ABCU])\s+([A-Z]\-[A-Z])\s+(\d{9})\s+(\d{2})\s+((?:\w| )+?)\s{2,}((?:\w| |\d)+?)\s{2,}(\d{5})\s((?:\w| )+?)\s{2,}((?:\w| )+?)\s{2,}
+    regex = (
+        rf"({di})",
+        AT_LEAST_ONE_SPACE,
+        rf"({sec})",
+        AT_LEAST_ONE_SPACE,
+        rf"({m})",
+        AT_LEAST_ONE_SPACE,
+        rf"({let})",
+        AT_LEAST_ONE_SPACE,
+        rf"({code})",
+        AT_LEAST_ONE_SPACE,
+        rf"({num})",
+        AT_LEAST_ONE_SPACE,
+        rf"({local_name})",
+        AT_LEAST_TWO_SPACES,
+        rf"({street})",
+        AT_LEAST_TWO_SPACES,
+        rf"({postal_code})",
+        AT_LEAST_ONE_SPACE,
+        rf"({town})",
+        AT_LEAST_TWO_SPACES,
+        rf"({province})",
+        AT_LEAST_TWO_SPACES,
+    )
+    return "".join(regex)
+    
+
+
+if __name__ == "__main__":
+    text = get_text_from_pdf("boletin.pdf")
+    regex = get_regex()
+    print(re.findall(regex, text)[:3])
